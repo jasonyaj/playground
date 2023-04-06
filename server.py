1d7144326ff729114d20e55ca82856907f16b459
jasonyaj <jason.yaj@gmail.com>
2023-04-06 09:39:26 -0500

    updated to render off one template, index

--- server.py
+++ server.py
@@ -4,19 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', times = 0, color="")
 
 @app.route('/play')
 def play():
-    return render_template('play.html', times=3)
+    return render_template('index.html', times = 3, color = 'blue')
 
 @app.route('/play/<int:num>')
 def multiply(num):
-    return render_template('num.html', times=num)
+    return render_template('index.html', times=num, color = 'blue')
 
 @app.route('/play/<int:num>/<string:color>')
 def change_color(num, color):
-    return render_template('color.html', times=num, color=color)
+    return render_template('index.html', times=num, color=color)
 
 if __name__=='__main__':
     app.run(debug=True)
